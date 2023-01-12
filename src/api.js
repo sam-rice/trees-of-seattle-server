81b61d9caf49b859e1d973ab22fe24bf1f4a27cb
Sam Rice <samsrice@gmail.com>
2023-01-12 15:04:01 -0800

    add POST request

--- src/api.js
+++ src/api.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-const trees = require("../data/testData.json")
+let trees = require("../data/testData.json")
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*")
@@ -21,6 +21,20 @@ app.listen(app.get("port"), () => {
 
 app.get("/v1/trees", (req, res) => {
   res.status(200).json(trees)
+})
+
+app.post("/v1/trees", (req, res) => {
+  const requiredParams = ["id", "speciesCommon", "speciesSci", "height", "circ", "age", "author", "img", "address", "neighborhood", "lat", "long"]
+  const newTree = req.body
+  console.log(newTree)
+
+  for (let requiredParam of requiredParams) {
+    if(!newTree[requiredParam]) {
+      return res.status(422).json({message: `You are missing a required parameter of ${requiredParam}`})
+    }
+    trees = [...trees, newTree]
+    return res.status(201).json(newTree)
+  }
 })
 
 // app.use("/.netlify/functions/api", router)
