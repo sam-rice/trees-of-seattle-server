f5fe3f6bd547177aed916cfb61f19fc5bdf25e6d
Sam Rice <samsrice@gmail.com>
2023-01-15 09:28:07 -0800

    preserve multer version of server

--- src/api.js
+++ src/api.js
@@ -1,13 +1,34 @@
 const express = require("express")
 const cors = require("cors")
+const multer = require("multer")
 // const serverless = require("serverless-http")
 
-const app = express()
 // const router = express.Router()
+const app = express()
+
 app.use(express.json())
 app.use(cors())
 
 let trees = require("../data/testData.json")
+
+// ---------------MULTER--------------------
+
+const path = require("path")
+
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../data/images/"))
+  },
+  filename: (req, file, cb) => {
+    console.log("from multer", file)
+    cb(null, file.originalname)
+  },
+})
+
+const upload = multer({ storage: storage })
+
+// ---------------ROUTES--------------------
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*")
@@ -24,7 +45,7 @@ app.get("/v1/trees", (req, res) => {
 })
 
 app.post("/v1/trees", (req, res) => {
-  const requiredParams = ["speciesCommon", "speciesSci", "height", "circ", "age", "author", "img", "address", "neighborhood", "lat", "long"]
+  const requiredParams = ["speciesCommon", "speciesSci", "isNative", "height", "circ", "age", "author", "address", "neighborhood", "lat", "long", "img"]
   const newTree = {
     id: Date.now(),
     ...req.body
@@ -34,9 +55,20 @@ app.post("/v1/trees", (req, res) => {
     if (!newTree[requiredParam]) {
       return res.status(422).json({ message: `You are missing a required parameter of ${requiredParam}` })
     }
+
+    console.log("from POST", newTree)
     trees = [...trees, newTree]
     return res.status(201).json(newTree)
   }
+})
+
+app.post("/v1/trees/photo", upload.single("img"), (req, res) => {
+
+    return res.status(201).json({ message: "success!" })
+})
+
+app.get("/v1/trees/pics", (req, res) => {
+  // res.status(200).json(trees)
 })
 
 // app.use("/.netlify/functions/api", router)
