066a331227e9c80c01c6d3c0e7a267063332d2c3
Sam Rice <samsrice@gmail.com>
2023-01-15 22:35:50 -0800

    fix POST returned tree object bug

--- controller/trees.js
+++ controller/trees.js
@@ -12,9 +12,8 @@ class TreesController {
   }
 
   async createTree(req, res) {
-
     const requiredParams = ["speciesCommon", "speciesSci", "address", "age", "author", "lat", "long"]
-    console.log(req.body)
+
     for (let requiredParam of requiredParams) {
       if (!req.body[requiredParam]) {
         return res.status(422).json({ message: `You are missing a required parameter of "${requiredParam}".` })
@@ -22,8 +21,10 @@ class TreesController {
     }
 
     try {
-      const id = await treesService.createTree(req.body)
-      res.status(201).json(id)
+      const { id } = await treesService.createTree(req.body)
+      const allTrees = await treesService.getTrees()
+      const newTree = allTrees.find(tree => tree.id === id)
+      res.status(201).json(newTree)
     } catch (err) {
       console.error(err)
       res.status(500).json("Failed to add new tree.")
