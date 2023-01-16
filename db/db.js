07ea75f8a83328154e3c2ec2d6089556b2183444
Sam Rice <samsrice@gmail.com>
2023-01-16 13:09:35 -0800

    change db config

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
- const knex = require("knex")
-const { propfind } = require("../routes")
- const knexfile = require("./knexfile")
+const knex = require("knex")
+// const { propfind } = require("../routes")
+const knexfile = require("./knexfile")
 
 //  TODO in prod, use dependency injection
 //  to create knex instance so db access can be mocked
@@ -9,6 +9,6 @@ const { propfind } = require("../routes")
 // TODO in prod don't access knexfile.development directly, 
 // instead decide with env vars which config to use
 
- const db = knex(knexfile.development)
+ const db = knex(knexfile.production)
 
  module.exports = db
