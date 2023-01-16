93a6fd6a3bc21ef3b9062f5652afe9f6dfdcb796
Sam Rice <samsrice@gmail.com>
2023-01-16 13:28:30 -0800

    update enviroment configs

--- knex.js
+++ knex.js
@@ -1,5 +1,6 @@
 const knex = require("knex")
-const config = require("./knexfile")
-const enviroment = "production"
-const knexConfig = config[enviroment]
+const config = require("./db/knexfile")
+const enviroment = process.env.NODE_ENV || "development"
+// const enviroment = "development" -- previously this
+const knexConfig = config[production]
 module.exports = knex(knexConfig)
