d3440cbf545d0850dbf3a548399256f8a12dfa1b
Sam Rice <samsrice@gmail.com>
2023-01-16 15:07:36 -0800

    update Procfile and knexfile for deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node index.js
+web: npx knex migrate:rollback --knexfile ./db/knexfile.js && npx knex migrate:latest --knexfile db/knexfile.js && npx knex seed:run --knexfile db/knexfile.js && node index.js
--- db/knexfile.js
+++ db/knexfile.js
@@ -27,7 +27,10 @@ module.exports = {
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: __dirname + "/db/migrations"
+    },
+    seeds: {
+      directory: __dirname + "/db/seeds"
     }
   }
 }
