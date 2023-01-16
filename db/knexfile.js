7348039691ba582bc2949cfe7222970fcf8d7f05
Sam Rice <samsrice@gmail.com>
2023-01-16 11:55:25 -0800

    add heroku and ready branch for remote

--- db/knexfile.js
+++ db/knexfile.js
@@ -3,14 +3,43 @@
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+// module.exports = {
+
+//   development: {
+//     client: 'postgresql',
+//     connection: {
+//       database: 'trees_of_seattle',
+//       user:     'postgres',
+//       password: 'himalayan4316'
+//     },
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//       tableName: 'knex_migrations'
+//     }
+//   }
+// };
+
 module.exports = {
 
   development: {
-    client: 'postgresql',
+    client: 'pg',
     connection: {
-      database: 'trees_of_seattle',
-      user:     'postgres',
-      password: 'himalayan4316'
+      user: 'postgres',
+      password: 'himalayan4316',
+      database: 'trees_of_seattle'
+    }
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
     },
     pool: {
       min: 2,
@@ -20,4 +49,4 @@ module.exports = {
       tableName: 'knex_migrations'
     }
   }
-};
+}
--- index.js
+++ index.js
@@ -7,4 +7,6 @@ app.use(express.json())
 app.use(router)
 app.use(cors())
 
+app.set("port", 3001)
+
 app.listen(3001, () => console.log(`server is now running on port 3001!`))
